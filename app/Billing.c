7826c960c0dd73661e5501e309293dfcaca11e9d
jlinares <julien@2linares.fr>
2026-03-03 15:45:07 +0100

    generate config file

--- app/Billing.c
+++ app/Billing.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-struct
+typedef struct
 {
     char BusinessName[50];
     char BusinessAddress[100];
@@ -11,19 +14,19 @@ struct
     char BillingNumber[20];
 } Header;
 
-struct
+typedef struct
 {
     char legalNotice[200];
     char paymentTerms[100];
 } Footer;
 
-struct
+typedef struct
 {
     char name[50];
     char address[100];
 } Customer;
 
-struct
+typedef struct
 {
     char description[100];
     int quantity;
@@ -31,7 +34,7 @@ struct
     float totalPrice;
 } Item;
 
-struct
+typedef struct
 {
     char IBAN[34];
     char BIC[11];
@@ -42,10 +45,61 @@ struct
 
 
 
-f
+bool isConfigFileValid()
+{
+    FILE *file = fopen("config.txt", "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+    fclose(file);
+    return true;
+}
+
+void generateConfigFile()
+{
+    FILE *file = fopen("config.txt", "w");
+    if (file == NULL)
+    {
+        printf("Error creating configuration file.\n");
+        return;
+    }
+    Header header = {
+        .BusinessName = "default",
+        .BusinessAddress = "default",
+        .BusinessPhone = "+0000000000",
+        .BussinessIdentificationNumber = "000000000000",
+        .BusinessEmail = "default@default.default"};
+    printf("Enter Business Name: ");
+    scanf("%s", header.BusinessName);
+
+    printf("Enter Business Address: ");
+    scanf("%s", header.BusinessAddress);
+
+    printf("Enter Business Phone (format: +0000000000): ");
+    scanf("%s", header.BusinessPhone);
+
+    printf("Enter Business Identification Number (SIRET in France): ");
+    scanf("%s", header.BussinessIdentificationNumber);
+
+    printf("Enter Business Email: ");
+    scanf("%s", header.BusinessEmail);
+
+
+    fprintf(file, "[Header]\n");
+    fprintf(file, "BusinessName=%s\n", header.BusinessName);
+    fprintf(file, "BusinessAddress=%s\n", header.BusinessAddress);
+    fprintf(file, "BusinessPhone=%s\n", header.BusinessPhone);
+    fprintf(file, "BussinessIdentificationNumber=%s\n", header.BussinessIdentificationNumber);
+    fprintf(file, "BusinessEmail=%s\n", header.BusinessEmail);
+    fclose(file);
+}
 
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    if (!isConfigFileValid())
+    {
+        generateConfigFile();
+    }
+    
 }
